02a77fbc016b9d38c5d98619ee2896b213400695
HeYuanHang <3145083560@qq.com>
2021-05-26 14:27:58 +0800

    first commit

---
+++ src/main/java/test/gitTest.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Author 何远航
+ * @Date: 2021/5/26 14:25
+ * @Version 1.8
+ */
+public class gitTest {
+    public static void main(String[] args){
+        System.out.println("测试git以及gitHub");
+    }
+}
